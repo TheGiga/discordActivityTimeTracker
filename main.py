a6e7eec038030503b0331c47475100b35d57e1e8
TheGiga <gigabitppl@gmail.com>
2024-07-17 15:54:07 +0300

    gg

--- main.py
+++ main.py
@@ -185,7 +185,10 @@ async def channel_name_loop():
     await channel.edit(name=f'casino game hours: {game_data.overall_time / 60:.2f}')
 
 
-@has_permissions(move_members=True)
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(content=error)
+
 @bot.slash_command(name='move_all')
 async def move_all_command(
         ctx: discord.ApplicationContext,
@@ -206,7 +209,6 @@ async def move_all_command(
     await ctx.send_followup("✅ Done!")
 
 
-@has_permissions(move_members=True)
 @bot.slash_command(name='move_fast')
 async def fast_move_command(
         ctx: discord.ApplicationContext,
